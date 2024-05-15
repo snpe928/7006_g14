507a845c1bad6b540ea1134ca071d582d836046d
snpe928 <166684316+snpe928@users.noreply.github.com>
2024-05-15 16:28:53 +0800

    Update 7006_Group14.py

--- 7006_Group14.py
+++ 7006_Group14.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 #import seaborn as sns
 #from sklearn.preprocessing import StandardScaler
 #from sklearn.ensemble import GradientBoostingClassifier
