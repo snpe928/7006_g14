3308e757b5e5f72ea1966b5e39598709bb761478
snpe928 <166684316+snpe928@users.noreply.github.com>
2024-05-15 17:43:48 +0800

    Update 7006_Group14.py

--- 7006_Group14.py
+++ 7006_Group14.py
@@ -5,11 +5,11 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, mean_squared_error, classification_report
-from imblearn.over_sampling import SMOTE
+#from sklearn.preprocessing import StandardScaler
+#from sklearn.ensemble import GradientBoostingClassifier
+#from sklearn.model_selection import train_test_split
+#from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score, f1_score, mean_squared_error, classification_report
+#from imblearn.over_sampling import SMOTE
 from scipy.stats import skew, kurtosis
 
 # Set page configuration
